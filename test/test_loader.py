a104e961a92eef7a913d2b603b44efe34e8e2e94
Jindrich Smitka <jsmitka@smita.info>
2014-08-26 12:28:14 +0200

    Added storage to template loader

--- test/test_loader.py
+++ test/test_loader.py
@@ -1,8 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 import pytest
+import six
+
+if six.PY3:
+    from unittest.mock import patch, call, MagicMock
+else:
+    from mock import patch, call, MagicMock
 
 from jinja2_stl.loader import FilestorageTemplateLoader
 
 
 class TestFilestorageTemplateLoader(object):
-    pass
+    def test_storage_saved_on_init(self):
+        storage = MagicMock()
+        loader = FilestorageTemplateLoader(storage=storage)
+
+        assert loader._storage == storage
