6cfa41ef92afd95915a22fdb265c74a1f2a988c6
Jindrich Smitka <jsmitka@smita.info>
2014-08-26 12:38:57 +0200

    Added storage fixture

---
+++ conftest.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import six
+
+import pytest
+
+if six.PY3:
+    from unittest.mock import MagicMock
+else:
+    from mock import MagicMock
+
+
+@pytest.fixture
+def storage():
+    return MagicMock()
