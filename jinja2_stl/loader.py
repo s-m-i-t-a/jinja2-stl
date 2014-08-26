6a8c71d1f705a567f167209e5d5b63806da59b02
Jindrich Smitka <jsmitka@smita.info>
2014-08-26 12:36:18 +0200

    Added loader base class

--- jinja2_stl/loader.py
+++ jinja2_stl/loader.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from jinja2 import BaseLoader
 
-class FilestorageTemplateLoader(object):
+
+class FilestorageTemplateLoader(BaseLoader):
     def __init__(self, storage):
         self._storage = storage
--- test/test_loader.py
+++ test/test_loader.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import pytest
 import six
+import pytest
 
 if six.PY3:
     from unittest.mock import patch, call, MagicMock
 else:
     from mock import patch, call, MagicMock
 
+from jinja2 import BaseLoader
+
 from jinja2_stl.loader import FilestorageTemplateLoader
 
 
 class TestFilestorageTemplateLoader(object):
+    def test_template_loader_is_subclass_of_base_loader(self):
+        assert issubclass(FilestorageTemplateLoader, BaseLoader)
+
     def test_storage_saved_on_init(self):
         storage = MagicMock()
         loader = FilestorageTemplateLoader(storage=storage)
